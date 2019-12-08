45ffbde9527c74965033d11ce28bed5e81574f6a
David Cantoran <davidcantoran36@gmail.com>
2019-12-08 18:32:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    print key.to_s.capitalize + ":"
+    print key.to_s.capitalize + ": "
     value.each do |season, supply_array|
       print season.to_s.capitalize + ":"
       print supply_array.join(", ")
